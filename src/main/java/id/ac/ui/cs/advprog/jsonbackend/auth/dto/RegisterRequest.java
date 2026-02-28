98025f014967d08cbb3a9609953ec32bf7c4bd4f
rexy <rexy.adrian.f@gmail.com>
2026-03-01 04:56:54 +0700

    feat(auth): add dto for auth request and response

---
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/auth/dto/RegisterRequest.java
@@ -0,0 +1,5 @@
+package id.ac.ui.cs.advprog.jsonbackend.auth.dto;
+
+import id.ac.ui.cs.advprog.jsonbackend.auth.model.Role;
+
+public record RegisterRequest(String email, String password, Role role) {}
