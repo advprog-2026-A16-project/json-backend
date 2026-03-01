1f4624fce798877fd7081762f1f6d52ee7b97b9c
rexy <rexy.adrian.f@gmail.com>
2026-03-02 02:27:57 +0700

    feat(auth): add initial user service

---
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/auth/service/AuthServiceImpl.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.jsonbackend.auth.service;
+
+import id.ac.ui.cs.advprog.jsonbackend.auth.dto.AuthResponse;
+import id.ac.ui.cs.advprog.jsonbackend.auth.dto.LoginRequest;
+import id.ac.ui.cs.advprog.jsonbackend.auth.dto.RegisterRequest;
+
+public class AuthServiceImpl implements AuthService {
+    @Override
+    public AuthResponse register(RegisterRequest request) {
+        return null;
+    }
+
+    @Override
+    public AuthResponse login(LoginRequest request) {
+        return null;
+    }
+}
