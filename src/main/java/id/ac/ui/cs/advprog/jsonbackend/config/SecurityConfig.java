f37cbaa6b3afa7d5c11b3d00a90d09890efc1570
XxymbolL <rifqy.Kurniawan1703@gmail.com>
2026-03-06 17:27:14 +0700

    fix(security): allow OPTIONS and public GET products

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/config/SecurityConfig.java
@@ -2,6 +2,12 @@ package id.ac.ui.cs.advprog.jsonbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+<<<<<<< Updated upstream
+=======
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+>>>>>>> Stashed changes
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,10 +19,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+<<<<<<< Updated upstream
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
             );
+=======
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+>>>>>>> Stashed changes
         return http.build();
     }
 }
