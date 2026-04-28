5d81345f01228fa2fc5b04cd2dd76b87de0a6640
XxymbolL <rifqy.Kurniawan1703@gmail.com>
2026-04-28 21:14:14 +0700

    fix[GREEN]: prioritize inventory exception handler to return 403 for forbidden inventory access

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/inventory/exception/InventoryExceptionHandler.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/inventory/exception/InventoryExceptionHandler.java
@@ -1,12 +1,15 @@
 package id.ac.ui.cs.advprog.jsonbackend.inventory.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
-@RestControllerAdvice(basePackages = "id.ac.ui.cs.advprog.jsonbackend.inventory")
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class InventoryExceptionHandler {
 
     @ExceptionHandler(ForbiddenInventoryAccessException.class)
