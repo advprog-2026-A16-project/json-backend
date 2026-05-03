909eaeace4609821f1227206a755567ec24503c0
rexy <rexy.adrian.f@gmail.com>
2026-05-04 05:42:27 +0700

    feat(auth): add AuthOutboxEvent repository

---
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/auth/event/repository/AuthOutboxEventRepository.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.jsonbackend.auth.event.repository;
+
+import id.ac.ui.cs.advprog.jsonbackend.auth.event.AuthOutboxEventStatus;
+import id.ac.ui.cs.advprog.jsonbackend.auth.event.model.AuthOutboxEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.UUID;
+
+public interface AuthOutboxEventRepository extends JpaRepository<AuthOutboxEvent, UUID> {
+    List<AuthOutboxEvent> findTop50ByStatusOrderByCreatedAtAsc(AuthOutboxEventStatus status);
+}
