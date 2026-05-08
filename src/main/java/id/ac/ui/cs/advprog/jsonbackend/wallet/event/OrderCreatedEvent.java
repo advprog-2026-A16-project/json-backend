ce5b411c2b6c36cc467105ac0d4085ba82456e82
angelojohenry <johenry.angelo@gmail.com>
2026-05-08 16:30:10 +0700

    feat: add OrderCreatedEvent

---
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/wallet/event/OrderCreatedEvent.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.jsonbackend.wallet.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class OrderCreatedEvent {
+    private String orderId;
+    private UUID userId;
+    private BigDecimal amount;
+}
