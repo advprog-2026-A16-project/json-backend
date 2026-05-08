81718dc4f00d8ca5393c4a56be7f7143e45dc415
angelojohenry <johenry.angelo@gmail.com>
2026-05-08 16:33:23 +0700

    feat: add PaymentFailedEvent

---
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/wallet/event/PaymentFailedEvent.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.jsonbackend.wallet.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PaymentFailedEvent {
+    private String orderId;
+    private String reason;
+}
