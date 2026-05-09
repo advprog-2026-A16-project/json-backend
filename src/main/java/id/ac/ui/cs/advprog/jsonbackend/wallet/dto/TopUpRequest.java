fec9268356aacc993d5500a8100f7dbf4f3a9935
angelojohenry <johenry.angelo@gmail.com>
2026-05-09 21:47:33 +0700

    refactor: add validation in wallet dto

--- src/main/java/id/ac/ui/cs/advprog/jsonbackend/wallet/dto/TopUpRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonbackend/wallet/dto/TopUpRequest.java
@@ -2,11 +2,18 @@ package id.ac.ui.cs.advprog.jsonbackend.wallet.dto;
 
 import java.math.BigDecimal;
 import java.util.UUID;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
 @Setter
 public class TopUpRequest {
+    @NotNull(message = "User ID is required")
     private UUID userId;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", message = "Amount must be greater than zero")
     private BigDecimal amount;
 }
